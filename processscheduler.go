be44b308ca83a281b9dbb4d20b9fd8a5b1a4a659
Ethan Peterson <cap2010.ep@gmail.com>
2017-09-29 21:35:42 -0600

    Make skeleton for round-robin schedule mocker.

--- processscheduler.go
+++ processscheduler.go
@@ -8,6 +8,37 @@ import (
 var k = 5244%3 + 2 // 2
 var proc = make([]int, k)
 
+// circular simulates an on-line round-robin process scheduling procedure for a given job queue
+// que holds information as [1][2,3] where:
+//   1 is the queue position
+//   2 is the arrival time
+//   3 is the processing time required to finish
+func circular(que [][2]int) int {
+	tstart := que[0][0]
+	tcur := tstart
+
+	// initialize the processors
+	p1 := que[0]
+	p2 := que[1]
+	que = que[2:]
+	j := 1 // the processor upon which a process is run
+
+	for i := 0; i < len(que); {
+		// determine the processor to use
+		j = (j + 1) % k
+
+		// update time spent working on current process
+
+		// move the current process to the end of the queue if it is incomplete
+		//   do not increment list index
+
+		// begin working on next process
+
+	}
+
+	return tcur - tstart // return the time elapsed since start
+}
+
 // processscheduler simulates a multi-core, non-preemptive process scheduler
 func main() {
 	fmt.Println(len(proc))
