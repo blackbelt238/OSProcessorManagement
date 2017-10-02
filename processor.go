16fe6f16f8f5d979fff529a28ea0452f9b11036c
Ethan Peterson <cap2010.ep@gmail.com>
2017-10-01 19:01:59 -0600

    Define a processor fully.

--- processor.go
+++ processor.go
@@ -1,11 +1,21 @@
 package main
 
 type Processor struct {
-	Job   [2]int // the slot to hold the current job
-	tproc int    // the time elapsed since the processor was last reset
+	Job   *Job // the slot to hold the current job
+	tproc int  // the time elapsed since the processor was last reset
+}
+
+func (p *Processor) Elapse(t int) {
+	p.tproc += t
+}
+
+func (p *Processor) LoadProcess(pr *Job) {
+	p.Job = pr
+	p.tproc += ltime
 }
 
 func (p *Processor) Reset() {
+	p.Job = FillerJob()
 	p.tproc = 0
 }
 
